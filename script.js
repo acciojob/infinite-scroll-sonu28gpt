bad058b4104f67afd0b5e3ffb96d3f24e8c9ef9e
sonu28gpt <98702109+sonu28gpt@users.noreply.github.com>
2025-01-16 01:16:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here!
 
+let ol=document.getElementById('infi-list');
+let main=document.querySelector('main');
+let cnt=10;
+for(let i=0;i<10;i++){
+	ol.innerHTML+=`<li>
+		item${i+1}
+	</li>`;
+	}
+function render(){
+	
+	for(let i=cnt;i<cnt+2;i++){
+	ol.innerHTML+=`<li>
+		item${i+1}
+	</li>`;
+	}
+	cnt=cnt+2;
+}
+ol.addEventListener('scrollend',()=>{
+	render();
+})
+
